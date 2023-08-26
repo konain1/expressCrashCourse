fed611d6ad7c0ae1450dd6e47ed768fa31578e52
konain1 <kaunainkasab@gmail.com>
2023-08-26 13:03:15 +0530

    sendFile

--- express/index.js
+++ express/index.js
@@ -1,10 +1,19 @@
 
 const express = require('express')
-
+const path = require('path')
 const app = express()
+
+console.log(__dirname)
 
 app.get("/",(req,res,next)=>{
     res.send("hello world")
 })
+
+app.get('/files',(req,res,next)=>{
+
+    res.sendFile(path.join(__dirname, 'index.html'))
+
+})
+
 
 app.listen(8080)
