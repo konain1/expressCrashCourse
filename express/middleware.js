fb73c6ac25e593f8f630ba8b8a782d89ee94f3be
konain1 <kaunainkasab@gmail.com>
2023-08-27 20:26:49 +0530

    app.use(express.urlencoded({extended:false}))

--- express/middleware.js
+++ express/middleware.js
@@ -1,23 +1,39 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
+const mor = require('morgan')
 
 
 
 let logger = (req,res,next)=>{
+
     console.log(`${new Date()},  Requrest:[${req.method}] , [${req.url}]`)
+
     next()
 }
 
 
 
-app.use(logger)
+// app.use(mor('dev'))
 
-app.get('/home',(req,res)=>{
+// app.use(mor('common'))
+app.use(mor('short'))
+
+
+
+
+app.get('/',(req,res)=>{
     res.send("home")
 })
 
+app.get('/about',(req,res)=>{
+    res.send("about")
+})
+app.use(logger)
+
+app.get('/contact',(req,res)=>{
+    res.send("contact")
+})
 
 
 app.listen(8080)
---
+++ express/post.js
@@ -0,0 +1,44 @@
+
+
+const express = require('express')
+
+const app = express();
+const path = require('path')
+
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({extended:false}))
+
+let books = [
+    {id:1,title:'book 1',author:'author 1'},
+
+    {id:2,title:'book 2', author:'author 2'},
+    
+    {id:3,title:'book 3',author:'author 3'}
+]
+
+
+
+
+app.get('/books' , (req,res,next)=>{
+
+    res.json(books)
+
+    // res.sendFile(path.join(__dirname ,'public' ,'index.html'))
+})
+
+app.post('/books',(req,res,next)=>{
+
+    console.log(req.body)
+
+    const newBook = {
+        id: books.length +1,
+        title : req.body.title,
+        author: req.body.author
+    }
+
+})
+
+
+app.listen(8080)
