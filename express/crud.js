10d0ba6cbf03c2e164d6772fdf6d470fcab33072
konain1 <kaunainkasab@gmail.com>
2023-08-28 12:56:21 +0530

    delete method is working

--- express/crud.js
+++ express/crud.js
@@ -48,10 +48,31 @@ app.put('/book/:id',(req,res)=>{
 
     
     if(index !== -1){
+        // books[index] = {...books[index],...updatedData}
         books[index] = {...updatedData}
+
         res.json(books)
 
 
+    }else{
+        res.status(404).json(books)
+    }
+
+})
+
+app.delete('/book/:id',(req,res)=>{
+
+    const id = parseInt(req.params.id)
+
+    const index = books.findIndex( book => book.id === id)
+
+    const DeletedData = books[index]
+
+    if(index !== -1){
+        books = books.filter((i)=> {
+           return i.id !== id
+        })
+        res.json(books)
     }else{
         res.status(404).json(books)
     }
